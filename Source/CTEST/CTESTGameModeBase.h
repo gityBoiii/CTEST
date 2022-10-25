b674d540196aadd0423b6842010e59c4cb017938
gityBoii <gnewid88@gmail.com>
2022-10-25 17:07:20 +0900

    1

---
+++ Source/CTEST/CTESTGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CTESTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CTEST_API ACTESTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
